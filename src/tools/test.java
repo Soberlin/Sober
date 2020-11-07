9b838b4dd3b001074501b6815e9f2d2b076835e3
刘果 <liug@weipaitang.com>
2020-11-07 13:33:25 +0800

    2222

---
+++ src/tools/test.java
@@ -0,0 +1,16 @@
+package tools;
+
+import javax.xml.bind.SchemaOutputResolver;
+
+/**
+ * @author liug
+ * @version 1.0
+ * @date 2020/11/7 1:31 下午
+ */
+public class test
+{
+
+    public static void main(String[] args) {
+        System.out.println( "test");
+    }
+}
