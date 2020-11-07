65b4b6da9ea30b60d9cce4be2219ca95afc5ea46
刘果 <liug@weipaitang.com>
2020-11-07 13:38:49 +0800

    liug分支提交代码

---
+++ src/tools/Hello.java
@@ -0,0 +1,15 @@
+package tools;
+
+/**
+ * @author liug
+ * @version 1.0
+ * @date 2020/11/7 1:36 下午
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("11111");
+    }
+
+
+
+}
