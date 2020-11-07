56ce5a271eb807f5cd5837b9793f10903c41ca1f
刘果 <liug@weipaitang.com>
2020-11-07 13:21:35 +0800

    测试

---
+++ src/tools/TestHello.java
@@ -0,0 +1,15 @@
+package tools;
+
+/**
+ * @author liug
+ * @version 1.0
+ * @date 2020/11/7 11:15 上午
+ */
+public class TestHello {
+    public static void main(String[] args) {
+        System.out.println("测试环境中");
+    }
+
+
+
+}
